ae6b7a72f45666638871305c411fc2c73cb7ea03
jeff-lund <jdl3@pdx.edu>
2019-08-04 23:24:10 -0700

    moving things to header files

--- local/connect.h
+++ local/connect.h
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <error.h>
+#include <errno.h>
+#include <getopt.h>
+#include <fcntl.h>
+#include <wordexp.h>
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/resource.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#include "connect.h"
+
+#define SOCKPTH "/var/tmp/"
+#define SERVER_PATH "/tmp/splinter.sock"
+#define offsetof(TYPE, MEMBER) ((long)&((TYPE *)0)->MEMBER)
+
 void connect_splinter(void);
--- local/initialize.h
+++ local/initialize.h
@@ -1,2 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <error.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+#include "initialize.h"
+
+#define SPDIR ".splinter"
+#define MODE_DIR S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH
+#define MODE_FILE S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH
+#define N_P1 10
+#define N_P2 12
+#define N_P3 10
+#define CONFIG_STR "/.splinter"
+#define DAEMON "/splinter.daemon"
+
 int initialize(char *);
 char * makeName(void);
